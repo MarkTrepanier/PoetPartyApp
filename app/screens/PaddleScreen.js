bf244443c68e894eb07fc61f309198f9f53f1d91
Mark Trepanier <marktrepanier.cajigas@gmail.com>
2022-04-07 14:03:23 -0400

    adds button to change colors of paddle

--- app/screens/PaddleScreen.js
+++ app/screens/PaddleScreen.js
@@ -1,7 +1,22 @@
-import { View } from "react-native";
+import { View, Button } from "react-native";
+import { useState } from "react";
 
 function PaddleScreen(props) {
-  return <View style={{ backgroundColor: "blue", flex: 1 }}></View>;
+  const [color, setColor] = useState("blue");
+  const changeColor = () => {
+    color === "red" ? setColor("blue") : setColor("red");
+  };
+  return (
+    <View
+      style={{
+        backgroundColor: color,
+        flex: 1,
+        justifyContent: "flex-end",
+      }}
+    >
+      <Button title="color" onPress={changeColor} />
+    </View>
+  );
 }
 
 export default PaddleScreen;
