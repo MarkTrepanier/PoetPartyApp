f6058244a833fbc5b913a2b88b36a4c46a89b475
Mark Trepanier <marktrepanier.cajigas@gmail.com>
2022-05-02 15:13:10 -0400

    styles nav bar

--- App.js
+++ App.js
@@ -2,24 +2,52 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "@expo/vector-icons/AntDesign"
 
 import WordGenScreen from "./app/screens/WordGenScreen";
 import PaddleScreen from "./app/screens/PaddleScreen";
+import { backgroundColor } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
 
 const Tab = createBottomTabNavigator();
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator initialRouteName="WordGen" backgroundColor="blue">
+    <NavigationContainer
+    >
+      
+      <Tab.Navigator  style={{backgroundColor:'red'}}
+      initialRouteName="WordGen"
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+  
+          if (route.name === 'WordGen') {
+            iconName = focused
+              ? 'form'
+              : 'form';
+          } else if (route.name === 'Paddle') {
+            iconName = focused ? 'flag' : 'flag';
+          }
+  
+          // You can return any component that you like here!
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'blue',
+        tabBarInactiveTintColor: 'grey',
+        tabBarStyle:{
+          backgroundColor:'#f4f4f4',
+        }
+      })}
+      >
+        
         <Tab.Screen
           name="WordGen"
           component={WordGenScreen}
-          options={{ title: "WordGenn" }}
+          options={{ title: "WordGenn"}}
         />
         <Tab.Screen
           name="Paddle"
           component={PaddleScreen}
-          options={{ title: "Paddle" }}
+          options={{ title: "Paddle", }}
         />
       </Tab.Navigator>
 
